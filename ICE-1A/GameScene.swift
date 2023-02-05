240060ecabf1bf88f881f588655e49da68b387ef
WilsonMungai <wilsonmuguthi@gmail.com>
2023-02-05 17:39:38 -0500

    collision and sounds

--- ICE-1A/GameScene.swift
+++ ICE-1A/GameScene.swift
@@ -1,81 +1,131 @@
+import UIKit
+import AVFoundation
 import SpriteKit
 import GameplayKit
-import AVFoundation
 
 let screeSize = UIScreen.main.bounds
 var screenWidth: CGFloat?
 var screenHeight: CGFloat?
 
-class GameScene: SKScene {
-    
-    // Instance varaibles
-    var ocean1: Ocean?
+class GameScene: SKScene
+{
+    // instance variables
+    var ocean1 : Ocean?
     var ocean2 : Ocean?
     var player : Player?
-    var island: Island?
+    var island : Island?
+    var clouds : [Cloud] = []
     
-    override func sceneDidLoad() {
-        
+    override func sceneDidLoad()
+    {
         screenWidth = frame.width
         screenHeight = frame.height
         
-        name = "Game"
+        name = "GAME"
         
-        // add the ocean to the scene
+        // add ocean1 to the scene and starts it at the Reset location
         ocean1 = Ocean()
-        ocean1?.Start()
         ocean1?.Reset()
         addChild(ocean1!)
         
+        // add ocean2 to the scene and starts it lower
         ocean2 = Ocean()
-        ocean2?.Start()
         ocean2?.position.y = -627
         addChild(ocean2!)
         
-        // add the plane to the game scene
+        // add the player to the scene
         player = Player()
         addChild(player!)
         
-        // add the island to the game scene
+        // add the island to the scene
         island = Island()
         addChild(island!)
+        
+        // add 3 clouds to the scene
+        for _ in 0...2
+        {
+            let cloud: Cloud = Cloud()
+            clouds.append(cloud)
+            addChild(cloud)
+        }
+        
+        // Engine Sound
+        let engineSound = SKAudioNode(fileNamed: "engine.mp3")
+        addChild(engineSound)
+        engineSound.autoplayLooped = true
+        
+        
+        // preload / prewarm impulse sounds
+        do
+        {
+            let sounds: [String] = ["thunder", "yay"]
+            for sound in sounds
+            {
+                let path: String = Bundle.main.path(forResource: sound, ofType: "mp3")!
+                let url: URL = URL(fileURLWithPath: path)
+                let avPlayer: AVAudioPlayer = try AVAudioPlayer(contentsOf: url)
+                avPlayer.prepareToPlay()
+            }
+        }
+        catch
+        {
+            
+        }
     }
     
-    func touchDown(atPoint pos : CGPoint) {
+    
+    func touchDown(atPoint pos : CGPoint)
+    {
         player?.TouchMove(newPos: CGPoint(x: pos.x, y: -640))
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
+    func touchMoved(toPoint pos : CGPoint)
+    {
+        player?.TouchMove(newPos: CGPoint(x: pos.x, y: -640))
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint)
+    {
         player?.TouchMove(newPos: CGPoint(x: pos.x, y: -640))
     }
     
-    func touchUp(atPoint pos : CGPoint) {
-        player?.TouchMove(newPos: CGPoint(x: pos.x, y: -640))
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
-    // Gets triggered every frame(approx 60 frames per second/ once every 16 ms)
-    override func update(_ currentTime: TimeInterval) {
-        
+    // triggered every frame - once every 16ms
+    override func update(_ currentTime: TimeInterval)
+    {
         ocean1?.Update()
         ocean2?.Update()
         player?.Update()
         island?.Update()
+        
+        CollisionManager.SquaredRadiusCheck(scene: self, object1: player!, object2: island!)
+        
+        // update each cloud in clouds
+        for cloud in clouds
+        {
+            cloud.Update()
+            CollisionManager.SquaredRadiusCheck(scene: self, object1: player!, object2: cloud)
+        }
+        
     }
-    
 }
--- ICE-1A/GameViewController.swift
+++ ICE-1A/GameViewController.swift
@@ -4,42 +4,57 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var LivesLabel: UILabel!
+    @IBOutlet weak var ScoreLabel: UILabel!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        //print(self.view.frame.size.width)
+        //print(self.view.frame.size.height)
+
+        if let scene = GKScene(fileNamed: "GameScene")
+        {
             
-//            print(self.view.frame.width)
-//            print(self.view.frame.height)
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                
-                // Set the scale mode to scale to fit the window
+            if let sceneNode = scene.rootNode as! GameScene?
+            {
                 sceneNode.scaleMode = .aspectFill
                 
-                // Present the scene
-                if let view = self.view as! SKView? {
+                if let view = self.view as! SKView?
+                {
                     view.presentScene(sceneNode)
                     view.ignoresSiblingOrder = true
                 }
             }
         }
+        
+        // Initialize the Lives and Score
+        CollisionManager.gameViewController = self
+        ScoreManager.Score = 0
+        ScoreManager.Lives = 5
+        updateLivesLabel()
+        updateScoreLabel()
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
         return .portrait
-//        if UIDevice.current.userInterfaceIdiom == .phone {
-//            return .portrait
-//        } else {
-//            return .portrait
-//        }
     }
 
-    override var prefersStatusBarHidden: Bool {
+    override var prefersStatusBarHidden: Bool
+    {
         return true
+    }
+    
+    func updateScoreLabel()
+    {
+        ScoreLabel.text = "Score: \(ScoreManager.Score)"
+    }
+    
+    func updateLivesLabel()
+    {
+        LivesLabel.text = "Lives: \(ScoreManager.Lives)"
     }
 }
